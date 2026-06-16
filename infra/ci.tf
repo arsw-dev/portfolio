453c32b67118eb3d56a742f5bd591b0b5e55de1c
Aryan Swaminath <github@alias.arsw.dev>
2026-06-16 18:52:01 -0500

    feat(infra): tighten oidc trust policy - add terraform plan workflow

--- infra/ci.tf
+++ infra/ci.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:arsw-dev/portfolio:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:arsw-dev/portfolio:*"
           }
         }
       }
